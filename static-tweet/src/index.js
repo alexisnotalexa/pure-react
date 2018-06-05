3eb3239566cf301c5b0a9bbb6927bb24aed0a3ee
Alexis Okamura <alexiskokamura@gmail.com>
2018-06-05 10:37:27 -0700

    finished static-tweet exercise but fontawesome icons aren't working

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -1,8 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Time = () => (
+    <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button"/>
+);
+const RetweetButton = () => (
+    <i className="fa fa-retweet retweet-button"/>
+);
+const LikeButton = () => (
+    <i className="fa fa-heart like-button"/>
+);
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button"/>
+);
+
+function Avatar() {
+    return (
+        <img
+            src="https://www.gravatar.com/avatar/nothing" className="avatar"
+            alt="avatar" />
+    );
+}
+
+function Message() {
+    return (
+        <div className="message">
+            This is less than 140 characters.
+        </div>
+    );
+}
+
+function NameWithHandle() {
+    return (
+        <span className="name-with-handle">
+            <span className="name">Your Name</span>
+            <span className="handle">@yourhandle</span>
+        </span>
+    );
+}
+
+function Tweet() {
+    return (
+        <div className="tweet">
+            <Avatar />
+            <div className="content">
+                <NameWithHandle /><Time />
+                <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Tweet />,
+    document.querySelector('#root')
+);

79f202004dfb4ee14a034448d12168ba84d27dde
Alexis Okamura <alexiskokamura@gmail.com>
2018-06-06 16:21:48 -0700

    minor updates

---
+++ proptypes-tweet/src/index.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import './index.css';
+
+const Time = ({ time }) => {
+    const timeString = moment(time).fromNow();
+    return (
+        <span className="time">
+            {timeString}
+        </span>
+    );
+}
+
+Time.propTypes = {
+    time: PropTypes.string
+};
+
+const ReplyButton = () => (
+    <i className="fas fa-reply reply-button"/>
+);
+
+function Count(count) {
+    if(count > 0) {
+        return (
+            <span className="retweet-count">
+                {count}
+            </span>
+        );
+    } else {
+        return null;
+    }
+}
+
+const RetweetButton = ({ count }) => (
+    <span className="retweet-button">
+        <i className="fas fa-retweet" />
+        <Count count={count} />
+    </span>
+);
+
+const LikeButton = ({ count }) => (
+    <span className="like-button">
+        <i className="fas fa-heart" />
+        {
+            count > 0 &&
+            <span className="like-count">
+                {count}
+            </span>
+        }
+    </span>
+);
+
+LikeButton.propTypes = {
+    count: PropTypes.number
+};
+
+const MoreOptionsButton = () => (
+    <i className="fas fa-ellipsis-h more-options-button"/>
+);
+
+function Avatar({ hash }) {
+    var url = `https://www.gravatar.com/avatar/${hash}`;
+    return (
+        <img
+            src={url} 
+            className="avatar"
+            alt="avatar" />
+    );
+}
+Avatar.propTypes = {
+    hash: PropTypes.string
+};
+
+function Message({ text }) {
+    return (
+        <div className="message">
+            {text}
+        </div>
+    );
+}
+Message.propTypes = {
+    text: PropTypes.string
+};
+
+function NameWithHandle({ author }) {
+    const { name, handle} = author;
+    return (
+        <span className="name-with-handle">
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
+        </span>
+    );
+}
+
+NameWithHandle.propTypes = {
+    author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        handle: PropTypes.string.isRequired
+    })
+};
+
+function Tweet({ tweet }) {
+    return (
+        <div className="tweet">
+            <Avatar hash={tweet.gravatar} />
+            <div className="content">
+                <NameWithHandle author={tweet.author} />
+                <Time time={tweet.timestamp} />
+                <Message text={tweet.message} />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton count={tweet.retweets} />
+                    <LikeButton count={tweet.likes} />
+                    <MoreOptionsButton />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Comment({ author, message, likes }) {
+    return (
+        <div>
+            <div className="author">{author}</div>
+            <div className="message">{message}</div>
+            <div className="likes">
+                {likes > 0 ? likes : 'No'} likes
+            </div>
+        </div>
+    );
+}
+
+Comment.propTypes = {
+    message: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    likes: PropTypes.number
+}
+
+var testTweet = {
+    message: "Something about cats.", gravatar: "xyz",
+    author: {
+    handle: "catperson",
+    name: "IAMA Cat Person" },
+    likes: 2,
+    retweets: 0,
+    timestamp: "2016-07-30 21:24:37"
+}
+
+ReactDOM.render(
+    <Tweet tweet={testTweet}/>,
+    document.querySelector('#root')
+);
